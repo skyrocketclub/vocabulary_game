86e2b7348c7a70fb2c28b7836923c98ad99b3fce
ugo <kitisugochukwu@gmail.com>
2021-09-17 10:41:40 +0100

    Play game for default_mode implemented

--- Vocabulary_Game.cpp
+++ Vocabulary_Game.cpp
@@ -8,6 +8,8 @@
 #include <sstream>
 #include <vector>
 #include <iomanip>
+#include <cstdlib>
+#include <time.h>
 
 
 void display_menu();
@@ -20,9 +22,14 @@ bool check_word(std::string spanish_word);
 std::string capitalise(std::string);
 bool verify(std::string, std::string);
 bool default_verify(std::string, std::string);
+double score_percent( int ,int );
+void insert_no_highscore();
+size_t check_word_count();
+double play_default(int,std::string);
+double play_main(int);
 
 
-int option{ 1 };
+int option{ 1 };//universal variable...
 
 int main()
 {
@@ -83,11 +90,11 @@ void display_menu() {
 }
 
 void play_game() {
-    std::string player_name{};
-    int high_score{ 0 }, word_source{};
-
+    std::string player_name{}, h_score{};
+    int word_source{}; 
     system("CLS");
     std::cout << "WELCOME PLAYER\nEnter your nick_name:" << std::endl;
+    std::cin.ignore(1, '\n');
     std::getline(std::cin,player_name);
     player_name = capitalise(player_name);
 
@@ -100,7 +107,67 @@ void play_game() {
     {
         case 1:
         {
+            int attempts{ 0 };
+            double highscore{}, current_score{};
 
+            std::string h_score{}, substr{};
+            std::vector<std::string>result;
+            double current_highscore{};
+
+           
+            std::cout << player_name << " WELCOME\nCurrent Highscore: ";
+
+            std::ifstream in_file;
+            in_file.open("default_words.txt");
+ 
+            //first read out the high score...
+            std::getline(in_file, h_score);
+            std::stringstream s_stream(h_score);
+
+            while (s_stream.good()) {
+                std::getline(s_stream, substr, ',');
+                result.push_back(substr);
+            }
+            std::cout << result.at(0) << " with " << result.at(1)<<" %" << std::endl;
+            current_highscore = stod(result.at(1));
+            
+            //Now to the playing of the game..
+            std::cout << std::endl;
+            std::cout << "HOW MANY WORDS WOULD YOU LIKE TO ATTEMPT?\nOPTION: ";
+            std::cin >>attempts ;
+            in_file.close();
+
+            current_score = play_default(attempts,player_name);
+            if (current_score >= current_highscore) {
+                std::cout << "NEW HIGHSCORE!\n"<<player_name<<" : "<<current_score<<" %" << std::endl;
+                std::ifstream in_file;
+                
+                in_file.open("default_words.txt");
+                std::vector<std::string> lines;
+                std::string line{};
+                
+                while (std::getline(in_file, line)) {
+                    lines.push_back(line);
+                }
+                in_file.close();
+
+                std::ofstream out_file("temp.txt");
+                
+                out_file << player_name << "," << current_score << std::endl;
+                for (size_t i{ 1 }; i < lines.size(); i++) {
+                    out_file << lines.at(i) << std::endl;
+                }
+                out_file.close();
+                remove("default_words.txt");
+                rename("temp.txt", "default_words.txt");
+            }
+
+            else {
+                std::cout << "YOUR SCORE IS: " << current_score <<" %" << std::endl;
+            }
+
+            
+            
         }
         break;
 
@@ -113,12 +180,116 @@ void play_game() {
 
 }
 
-bool verify(std::string spanish, std::string english) {
+double play_default(int attempts,std::string ) {
+    std::string line{}, h_score{}, current_line{};
+    size_t game_size{};
+    int trial{ attempts }, pos;
+    double score{ 0 }, expected_score{}, percentage{};
 
+    expected_score = attempts * 10;
+
+    std::ifstream in_file;
+    in_file.open("default_words.txt");
+
+    std::vector<std::string>entry;
+    std::vector<std::string>default_failed;
+
+    std::getline(in_file, h_score);
+    while (std::getline(in_file, line)) {
+        entry.push_back(line);
+    }
+
+    game_size = entry.size();
+    std::cin.ignore(1, '\n');
+
+    while (trial > 0) {
+        srand(time(nullptr));
+        pos = rand() % game_size ; 
+        current_line = entry.at(pos);
+
+        std::string english{};
+        std::stringstream s_stream(current_line);
+        std::string substr{};
+        std::vector<std::string> result;
+
+        while (s_stream.good()) {
+            std::getline(s_stream, substr, ',');
+            result.push_back(substr);
+        }
+      
+
+        std::cout << result.at(0) << ": ";
+        std::getline(std::cin, english);
+        english = capitalise(english);
+        if (english == result.at(1)) {
+            score += 10;
+            std::cout << "\nCORRECT!\n\n";
+        }
+        else {
+            std::cout << "\nINCORRECT! (ANSWER: "<<result.at(1)<<"\n\n";
+        }
+
+        trial--;
+    }
+    percentage = (score / expected_score) * 100;
+
+    in_file.close();
+    return percentage;
+}
+
+double play_main(int attempts) {
+    double percent{};
+
+    return percent;
+}
+
+bool verify(std::string spanish, std::string english) {
+    bool status{ false };
+
+    return status;
 }
 
 bool default_verify(std::string spanish, std::string english) {
+    bool status{ false };
 
+    return status;
+}
+
+double score_percent(int attempts, int score) {
+    double percentage{ 0.0 };
+
+    return percentage;
+}
+
+void insert_no_highscore() {
+    size_t count{};
+    count = check_word_count();
+
+    if (count == 0) {
+        std::ofstream out_file("words.txt", std::ios::app);
+        out_file << "NO HIGH SCORE YET,0" << std::endl;
+        out_file.close();
+    }
+}
+
+size_t check_word_count() {
+    std::string line{};
+    std::vector <std::string> lines;
+    size_t value{};
+
+    //This creates the file incase it does not exist...
+    std::ofstream out_file("words.txt", std::ios::app);
+    out_file.close();
+
+    std::ifstream in_file;
+    in_file.open("words.txt");
+    while (std::getline(in_file, line)) {
+        lines.push_back(line);
+    }
+  
+    value = lines.size();
+    in_file.close();
+    return value;
 }
 
 void add_word() {
@@ -127,12 +298,16 @@ void add_word() {
 
     std::string spanish{}, english{};
     int number{};
+
+    insert_no_highscore();
     std::ofstream out_file("words.txt", std::ios::app);
+   
     
     if (!out_file) {
         std::cerr << "Error Opening File" << std::endl;
     }
     else {
+
         std::cout << "How many words would you like to add?" << std::endl<<"Option: ";
         std::cin >> number;             
         std::cin.ignore(1, '\n');
@@ -168,7 +343,7 @@ void add_word() {
 
 void view_words() {
     system("CLS");
-    std::string spanish{}, english{}, line{}, temp{};
+    std::string spanish{}, english{}, line{}, temp{}, high_score{};
     std::vector<std::string>entry;
    
 
@@ -179,6 +354,8 @@ void view_words() {
         std::cerr << "There are no words in you record at the moment" << std::endl;
     }
     else {
+        getline(in_file, high_score);
+
         std::cout << std::setw(20)<<std::left<<"SPANISH" <<"ENGLISH"<< std::endl<<std::endl;
         while (std::getline(in_file, line)) {
 
